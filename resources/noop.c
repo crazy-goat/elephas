825c9d3ec4bcf843910ede0447b013caf86cf98a
Hałas Piotr <halaspiotr@gmail.com>
2026-06-07 16:51:03 +0200

    fix: rename noop callback symbol to match FFI header expectation
    The C function was named elephas_noop_callback but the FFI header in
    NativeClient.php expects elephas_noop. This caused FFI\Exception when
    loading libelephas_noop.so.
    
    Also bumped CACHE_VERSION to v2 to invalidate previous cache that
    contained the library built with wrong symbol name.
    
    (closes #157)

--- resources/noop.c
+++ resources/noop.c
@@ -1,4 +1,4 @@
-void elephas_noop_callback(unsigned long ctx, void* packet, unsigned long long ts, const void* result, unsigned int result_size) {
+void elephas_noop(unsigned long ctx, void* packet, unsigned long long ts, const void* result, unsigned int result_size) {
     (void)ctx;
     (void)packet;
     (void)ts;
